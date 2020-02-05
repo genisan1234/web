1bd9cc26aeedc4209fa7eeae225c452271e553c0
jabrock <john.brock@oracle.com>
2020-02-04 19:51:35 -0800

    basic layout and components for departments page.

--- src/js/viewModels/departments.js
+++ src/js/viewModels/departments.js
@@ -5,11 +5,110 @@
  * @ignore
  */
 
-define(['accUtils'],
- function(accUtils) {
-
+define(
+  ['accUtils',
+    'jquery',
+    'knockout',
+    'ojs/ojarraydataprovider',
+    'ojs/ojcontext',
+    'ojs/ojknockouttemplateutils',
+    'ojs/ojdatagrid',
+    'ojs/ojfilmstrip',
+    'ojs/ojchart',
+    'ojs/ojpagingcontrol'],
+  function (accUtils, $, ko, ArrayDataProvider, Context, KnockoutTemplateUtils) {
     function DepartmentsViewModel() {
-      var self = this;
+      this.KnockoutTemplateUtils = KnockoutTemplateUtils;
+      const deptURL = 'https://apex.oracle.com/pls/apex/oraclejet/hr/departments/';
+      this.dataReady = ko.observable(false);
+
+      this.pagingModel = ko.observable();
+      this.depts = ko.observableArray();
+
+      this.getItemInitialDisplay = function (index) {
+        return index < 1 ? '' : 'none';
+      };
+
+      const tempData = [
+        {
+          "id": 0,
+          "series": "Series 1",
+          "group": "Group A",
+          "value": 42
+        },
+        {
+          "id": 1,
+          "series": "Series 2",
+          "group": "Group A",
+          "value": 55
+        },
+        {
+          "id": 2,
+          "series": "Series 3",
+          "group": "Group A",
+          "value": 36
+        },
+        {
+          "id": 3,
+          "series": "Series 4",
+          "group": "Group A",
+          "value": 22
+        },
+        {
+          "id": 4,
+          "series": "Series 5",
+          "group": "Group A",
+          "value": 22
+        }
+      ]
+      this.chartDataProvider = new ArrayDataProvider(tempData, {keyAttributes: 'id'});
+
+      this.dgDataProvider = ko.observable();
+      this.deptMap = ko.observable();
+      $.getJSON(deptURL).then((depts) => {
+        this.deptMap(new Map(Array.from(depts.items.map(dept => [dept.deptno, dept]))));
+        let tempDeptArray = [];
+        let tempArray = depts.items.map(dept => {
+          tempDeptArray.push({ name: dept.dname });
+          return {
+            deptno: dept.deptno,
+            dname: dept.dname,
+            loc: dept.loc
+          };
+        });
+        this.depts(tempDeptArray);
+        this.dgDataProvider(new ArrayDataProvider(tempArray, { keyAttributes: 'deptno' }));
+        this.dataReady(true);
+
+        let filmStrip = document.getElementById('deptFilmstrip');
+        let busyContext = Context.getContext(filmStrip).getBusyContext();
+        busyContext.whenReady().then(() => {
+          // Set the Paging Control pagingModel
+          this.pagingModel(filmStrip.getPagingModel());
+        });
+      })
+
+      this.getCellClassName = (cellContext) => {
+        let key = cellContext.keys.column;
+        if (key === 'deptno') {
+          return 'oj-helper-justify-content-right small-cell';
+        } else if (key === 'dname' ||
+          key === 'loc') {
+          return 'oj-sm-justify-content-flex-start med-cell';
+        }
+        return '';
+      };
+
+      this.getCellWidth = (cellContext) => {
+        let key = cellContext.data;
+        if (key === 'deptno') {
+          return 'width:20%';
+        } else if (key === 'dname' || key === 'loc') {
+          return 'width:40%';
+        }
+        return '';
+      }
+
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
 
@@ -21,7 +120,7 @@ define(['accUtils'],
        * and inserted into the DOM and after the View is reconnected
        * after being disconnected.
        */
-      self.connected = function() {
+      this.connected = () => {
         accUtils.announce('Department content loaded', 'assertive');
         document.title = "Intro - Departments";
         // Implement further logic if needed
@@ -30,7 +129,7 @@ define(['accUtils'],
       /**
        * Optional ViewModel method invoked after the View is disconnected from the DOM.
        */
-      self.disconnected = function() {
+      this.disconnected = () => {
         // Implement if needed
       };
 
@@ -38,7 +137,7 @@ define(['accUtils'],
        * Optional ViewModel method invoked after transition to the new View is complete.
        * That includes any possible animation between the old and the new View.
        */
-      self.transitionCompleted = function() {
+      this.transitionCompleted = () => {
         // Implement if needed
       };
     }
