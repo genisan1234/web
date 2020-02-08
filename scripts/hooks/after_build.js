a58fdaa25bf155965542dd457bceddb8a441d78b
donald Raikes <don.raikes@oracle.com>
2020-02-08 14:57:01 -0700

    Modified the after_build.js file to copy files to the docs subdirectory for github pages.

--- scripts/hooks/after_build.js
+++ scripts/hooks/after_build.js
@@ -11,41 +11,41 @@ module.exports = function (configObj) {
     console.log("Running after_build hook.");
 
     // if (configObj.buildType === 'release') {
-    // do something here to copy production files to /dist folder
-    // which can then be used with a dockerfile
-    fs.copy('./web/index.html', './dist/index.html', (err) => {
+    // do something here to copy production files to /docs folder
+    // which can then be used with a github pages
+    fs.copy('./web/index.html', './docs/index.html', (err) => {
       if (err) throw err;
       console.log('copied index.html file');
     });
-    fs.copy('./web/css', './dist/css', (err) => {
+    fs.copy('./web/css', './docs/css', (err) => {
       if (err) throw err;
       console.log('copied /css folder');
     });
-    fs.copy('./web/js/main.js', './dist/js/main.js', (err) => {
+    fs.copy('./web/js/main.js', './docs/js/main.js', (err) => {
       if (err) throw err;
       console.log('copied main.js file');
     });
-    fs.copy('./web/js/accUtils.js', './dist/js/accUtils.js', (err) => {
+    fs.copy('./web/js/accUtils.js', './docs/js/accUtils.js', (err) => {
       if (err) throw err;
       console.log('copied accUtils file');
     });
-    fs.copy('./web/js/appController.js', './dist/js/appController.js', (err) => {
+    fs.copy('./web/js/appController.js', './docs/js/appController.js', (err) => {
       if (err) throw err;
       console.log('copied appController file');
     });
-    fs.copy('./web/js/views', './dist/js/views', (err) => {
+    fs.copy('./web/js/views', './docs/js/views', (err) => {
       if (err) throw err;
       console.log('copied /views folder');
     });
-    fs.copy('./web/js/viewModels', './dist/js/viewModels', (err) => {
+    fs.copy('./web/js/viewModels', './docs/js/viewModels', (err) => {
       if (err) throw err;
       console.log('copied /viewModels folder');
     });
-    fs.ensureDir('./dist/js/libs', (err) => {
+    fs.ensureDir('./docs/js/libs', (err) => {
       if (err) throw err;
       console.log('created /js/libs folder');
     });
-    fs.copy('./web/js/libs/require', './dist/js/libs/require', (err) => {
+    fs.copy('./web/js/libs/require', './docs/js/libs/require', (err) => {
       if (err) throw err;
       console.log('copied requirejs folder');
     });
