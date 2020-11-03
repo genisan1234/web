a13e415d28fd3284f285f7e1a7dbee6f19282749
juryc <sanusaurav8082@gmail.com>
2020-11-03 13:14:36 +0530

    setting dashboard.js for src

--- application/src/js/viewModels/dashboard.js
+++ application/src/js/viewModels/dashboard.js
@@ -8,8 +8,8 @@
 /*
  * Your dashboard ViewModel code goes here
  */
-define(['accUtils'],
- function(accUtils) {
+define(['accUtils','knockout','jquery','ojs/ojarraydataprovider','ojs/ojlabel','ojs/ojselectsingle','ojs/ojchart','ojs/ojlistview'],
+ function(accUtils,ko,$,ArrayDataProvider) {
     function DashboardViewModel() {
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
@@ -22,6 +22,30 @@ define(['accUtils'],
        * and inserted into the DOM and after the View is reconnected
        * after being disconnected.
        */
+      var self = this;
+      var url = 'js/store_data.json';
+      self.activityDataProvider = ko.observable();
+      $.getJSON(url).then(function(data){
+              var activityarray  = data;
+              self.activityDataProvider(new ArrayDataProvider(activityarray,{keyAttributes:'id'}));
+      });
+      self.val = ko.observable('pie');
+      var chartTypes = [
+        { value: 'pie', label: 'Pie' },
+        { value: 'bar', label: 'Bar' }
+      ];
+      self.chartTypesProvider= new ArrayDataProvider(chartTypes,{keyAttributes:'value'});
+      var chartData = [
+        { "id": 0, "series": "Baseball", "group": "Group A", "value": 42 },
+        { "id": 1, "series": "Baseball", "group": "Group B", "value": 34 },
+        { "id": 2, "series": "Bicycling", "group": "Group A", "value": 55 },
+        { "id": 3, "series": "Bicycling", "group": "Group B", "value": 30 },
+        { "id": 4, "series": "Skiing", "group": "Group A", "value": 36 },
+        { "id": 5, "series": "Skiing", "group": "Group B", "value": 50 },
+        { "id": 6, "series": "Soccer", "group": "Group A", "value": 22 },
+        { "id": 7, "series": "Soccer", "group": "Group B", "value": 46 }
+      ];
+      self.chartDataProvider = new ArrayDataProvider(chartData,{keyAttributes:'id'});
       this.connected = () => {
         accUtils.announce('Dashboard page loaded.', 'assertive');
         document.title = "Dashboard";
